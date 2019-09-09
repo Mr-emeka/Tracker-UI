f115915b4239aaf53dd197a48d5c9f6706556e0c
Mr-emeka <speak2c.emeka@gmail.com>
2019-09-09 23:25:16 +0100

    fixed user dashboard

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,7 @@
 var makerequest = document.querySelector('.create');
 var modal = document.querySelector('.bg-modal');
 
-var editModal = document.querySelector('.edit-bg-modal');
 var close = document.querySelector('#cancel');
-var editClose = document.querySelector('#editcancel');
-var edit = document.querySelectorAll('.edit');
-edit.forEach(e => {
-  e.addEventListener('click', (e) => {
-    e.preventDefault();
-    editModal.style.display = "flex";
-  })
-})
 
 makerequest.addEventListener('click', (e) => {
   e.preventDefault();
@@ -20,9 +11,4 @@ makerequest.addEventListener('click', (e) => {
 close.addEventListener('click', (e) => {
   e.preventDefault();
   modal.style.display = "none";
-})
-
-editClose.addEventListener('click', (e) => {
-  e.preventDefault();
-  editModal.style.display = "none";
 })
